1df4f791204e6210b0862277c13864e60540f7ed
bertol26 <bertolfranco26@gmail.com>
2022-02-09 16:51:00 -0500

    primer commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const maquina4 = document.getElementById('description');
+
+const maquinaEscribir3 = (text = '',tiempo = 2000, etiqueta = '') => {
+    text = maquina4.innerHTML;
+    let arrayCaracteres = text.split('')
+    etiqueta.innerHTML = ''
+    let i = 0
+    let j = text.length
+    let escribir = setInterval(function(){
+      if (i === arrayCaracteres.length) {
+        etiqueta.innerHTML = text.substring(0,j)
+        
+        if (j === 0) {
+          etiqueta.innerHTML = ''
+          i = 0
+          j = text.length
+        }
+      } else {
+        etiqueta.innerHTML += arrayCaracteres[i]
+        i++
+      }
+    }, tiempo)
+  }
+
+
+  maquinaEscribir3("",50, maquina4)
